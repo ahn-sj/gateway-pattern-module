64f8e5078e3448a1d7d8250f13a7f1b4fa93c168
ahn-sj <jaesa5221@gmail.com>
2024-11-21 20:52:10 +0900

    feat: add http client

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ go get github.com/confluentinc/confluent-kafka-go
 go get github.com/go-resty/resty/v2
 go get github.com/bytedance/sonic
 ```
+
+### 직렬화 / 역직렬화, ObjectMapper 구현
--- app/client/root.go
+++ app/client/root.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"fmt"
 	"gateway-module/common"
 	"gateway-module/config"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
@@ -21,14 +22,116 @@ type HttpClient struct {
 func NewHttpClient(
 	cfg config.App,
 	producer map[string]kafka.Producer,
-) {
-	batchTime := cfg.Http.BatchTime
+) HttpClient {
+	batchTime := cfg.Producer.BatchTime
 
 	if batchTime == 0 {
 		batchTime = _defaultBatchTime
 	}
 
+	if cfg.Http.BaseUrl == "" {
+		panic("BaseUrl is required")
+	}
+
 	client := resty.New().
-		SetJSONMarshaler(common.JsonHandler.Marshal).    // sonic Marshal
-		SetJSONUnmarshaler(common.JsonHandler.Unmarshal) // sonic Unmarshal
+		SetJSONMarshaler(common.JsonHandler.Marshal).     // sonic Marshal
+		SetJSONUnmarshaler(common.JsonHandler.Unmarshal). // sonic Unmarshal
+		SetBaseURL(cfg.Http.BaseUrl)
+	return HttpClient{
+		cfg:      cfg,
+		client:   client,
+		producer: producer[cfg.App.Name],
+	}
+}
+
+func (h HttpClient) Get(url string, router config.Router) (interface{}, error) {
+	var buffer interface{} // 외부 호출한 결과를 받을 변수
+	var err error
+	var req *resty.Request
+	var resp *resty.Response
+
+	// defer // 함수가 종료할 때 실행
+
+	req = getRequest(h.client, router).SetResult(&buffer)
+	resp, err = req.Get(url)
+
+	fmt.Println(resp)
+
+	if err != nil {
+		return nil, err
+	}
+	return buffer, nil
+}
+
+func (h HttpClient) Post(url string, router config.Router, requestBody interface{}) (interface{}, error) {
+	var buffer interface{} // 외부 호출한 결과를 받을 변수
+	var err error
+	var req *resty.Request
+	var resp *resty.Response
+
+	// defer // 함수가 종료할 때 실행
+
+	req = getRequest(h.client, router).SetResult(&buffer).SetBody(requestBody)
+	resp, err = req.Post(url)
+
+	fmt.Println(resp)
+
+	if err != nil {
+		return nil, err
+	}
+	return buffer, nil
+}
+
+func (h HttpClient) Put(url string, router config.Router, requestBody interface{}) (interface{}, error) {
+	var buffer interface{} // 외부 호출한 결과를 받을 변수
+	var err error
+	var req *resty.Request
+	var resp *resty.Response
+
+	// defer // 함수가 종료할 때 실행
+
+	req = getRequest(h.client, router).SetResult(&buffer).SetBody(requestBody)
+	resp, err = req.Put(url)
+
+	fmt.Println(resp)
+
+	if err != nil {
+		return nil, err
+	}
+	return buffer, nil
+}
+
+func (h HttpClient) Delete(url string, router config.Router, requestBody interface{}) (interface{}, error) {
+	var buffer interface{} // 외부 호출한 결과를 받을 변수
+	var err error
+	var req *resty.Request
+	var resp *resty.Response
+
+	// defer // 함수가 종료할 때 실행
+
+	req = getRequest(h.client, router).SetResult(&buffer).SetBody(requestBody)
+	resp, err = req.Delete(url)
+
+	fmt.Println(resp)
+
+	if err != nil {
+		return nil, err
+	}
+	return buffer, nil
+}
+
+func getRequest(client *resty.Client, router config.Router) *resty.Request {
+	// h.client.R().SetAuthScheme().SetAuthToken().SetHeaders()
+	req := client.R().EnableTrace()
+
+	if router.Auth != nil {
+		if len(router.Auth.Key) != 0 {
+			req.SetAuthScheme(router.Auth.Key)
+		}
+		req.SetAuthScheme(router.Auth.Token)
+	}
+	if router.Header != nil {
+		req.SetHeaders(router.Header)
+	}
+	return req
 }
